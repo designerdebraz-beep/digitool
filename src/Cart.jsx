c5b1ec8071dbceb247d2948c9193c4348997d32b
designerdebraz-beep <designerdebraz@gmail.com>
2026-03-31 00:51:02 +0300

    styles added to the cart

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -21,13 +21,13 @@ const Cart = ({ cart,setcart }) => {
           </div>
            :  <div className='px-10'>
             
-            <h2 className='text-4xl'>Your cart {cart.length}</h2>
-            <div className="border-2 border-amber-100 ">
+            <h2 className='text-4xl py-3 font-bold'>Your cart {cart.length}</h2>
+            <div className="">
 
           
 
                 {
-                    cart.map(product => <div key={product.title} className="text-4xl mb-6 flex items-center justify-between gap-2 py-7 border-2 border-amber-950-100 rounded-lg px-4">
+                    cart.map(product => <div key={product.title} className="text-4xl mb-6 flex items-center justify-between gap-2 py-7 rounded-lg px-4 shadow-2xl">
                         <div className='flex items-center justify-center'>
                             <h2>{product.icon}</h2>
                         <h3 className="text-xl font-bold text-gray-900 mb-3">{product.title}</h3>
@@ -39,17 +39,17 @@ const Cart = ({ cart,setcart }) => {
                             {<h3 className="text-xl font-bold text-gray-900 mb-3">${product.price}</h3>}
                         </div>
                 
-                <MessageSquareX onClick={()=> handleDelete(product)} className='text-red-600'/>
+                <MessageSquareX onClick={()=> handleDelete(product)} className='text-red-600 cursor-pointer'/>
                     </div>)
 
                 }
 
-                 <div className='flex items-center justify-between bg-amber-100'>
+                 <div className='flex items-center justify-between bg-blue-400 px-2.5 py-2.5 text-white rounded-2xl'>
                     <h2 className='text-3xl font-bold'>Total: </h2>
                     <span className='text-5xl px-5'>${totalPrice}</span>
                  </div>
             </div>
-            <button onClick={handlePayment} className='btn btn-primary w-full mt-2'>Process to checkOut</button>
+            <button onClick={handlePayment} className='btn  w-full mt-2 bg-[#7C3AED] font-bold text-xl text-white py-5'>Process to checkOut</button>
         </div>}
        </>
     );
